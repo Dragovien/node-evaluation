edb0bbc81085b94e27c9f856bf9fd140fa932d52
Axel Hégron <axel.hegron@3wa.io>
2023-10-27 15:32:04 +0200

    dates formatted

--- server.js
+++ server.js
@@ -1,119 +1,102 @@
 
-import {createServer} from 'node:http'
-import { createReadStream, appendFileSync} from "node:fs";
+import { createServer } from 'node:http'
+import { createReadStream, readFileSync } from "node:fs";
 import queryString from 'node:querystring';
-import {students} from './Data/students.js'
-import { formattedArray } from './utils.js';
+import { students } from './Data/students.js'
+import { formattedArray, verifyInput } from './utils.js';
 import dotenv from 'dotenv';
 dotenv.config()
-const {APP_ENV, APP_PORT, APP_LOCALHOST} = process.env
+const { APP_ENV, APP_PORT, APP_LOCALHOST } = process.env
 
 const server = createServer(async (req, res) => {
 
-  let url = `${APP_LOCALHOST}:${APP_PORT}`
+  const { method } = req
 
-    const {method} = req
+  if (method === 'GET') {
 
-    if(method === 'GET') {
+    if (req.url === '/favicon.ico') {
+      res.writeHead(200, {
+        "Content-type": 'image/ico'
+      })
+      res.end()
+      return
+    }
 
-      if (req.url === '/favicon.ico') {
-          res.writeHead(200, {
-              "Content-type": 'image/ico'
-          })
-          res.end()
-          return
-      }
+    if (req.url === '/style') {
+      const css = readFileSync('./assets/css/style.css', 'utf8')
+      res.writeHead(200, { 'Content-type': 'text/css' })
+      res.end(css)
+      return
+    }
 
-      if (req.url === '/style') {
-        const css = readFileSync('./assets/css/style.css', 'utf8')
-        res.writeHead(200, {'Content-type': 'text/css'})
-        res.end(css)
-        return
-      }
+    if (req.url === '/') {
+      res.writeHead(200, {
+        "Content-type": 'text/html'
+      })
+      createReadStream('./view/home.html').pipe(res)
 
-      if (req.url === '/') {
-        res.writeHead(200, {
-            "Content-type": 'text/html'
-        })
-        createReadStream('./view/home.html').pipe(res)
+    } else if (req.url === '/users') {
+      res.writeHead(200, { 'content-type': 'text/html' })
 
-      } else if(req.url === '/users') {
-        res.writeHead(200, {'content-type': 'text/html'})
+      let html = '<ul class="userList" >'
 
-        let html = '<ul>'
-
-        students.map(student => {
-          html += `<li>
+      formattedArray(students).map(student => {
+        html += `<li class="userItem" >
             <p>${student.name} ${student.birth}</p>
             <button>X</button>
           </li>`
-        })
-        html += '</ul>'
+      })
+      html += '</ul>'
 
-        res.end(`
+      res.end(`
         <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
             <title>Les étudiant</title>
+            <link href="/style" rel="stylesheet" type="text/css" />
         </head>
         <body>
-          <h1>Étudiants</h1>
-          ${html}
-          <a href='/'>Home</a>
+        <header>
+          <nav class="nav">
+            <ul class="navList">
+              <li class="navItem"><a type="submit" class="" href="/">Home</a></li>
+              <li class="navItem"><a type="submit" class="" href="/users">Users</a></li>
+            </ul>
+          </nav>
+        </header>
+          <main>
+            <div class='container'>
+              <h1>Liste des étudiants</h1>
+                ${html}
+            </div> 
+          </main>
         </body>
         `)
-      } else {
-        res.writeHead(200, {
-          "Content-type": 'text/html'
+    } else {
+      res.writeHead(200, {
+        "Content-type": 'text/html'
       })
       createReadStream('./view/404.html').pipe(res)
-      }
-    } else if(method === 'POST') {
-      if (req.url === '/') {
-        let body = '';
-  
-        req.on('data', (chunk) => {
-          body += chunk.toString();
-        });
-  
-        req.on('end', () => {
-
-          const formData = queryString.parse(body);
-          const name = formData.name;
-          const birth = formData.birth
-
-          if(
-            name?.trim() === '' 
-            || birth?.trim() === '' 
-            || name?.trim() === '' && birth?.trim()
-          ) {
-            let message = ''
-            if(name?.trim() === '' && birth?.trim() === '') {
-              message = 'les 2 champs'
-            } else {
-              message = name?.trim() === '' ? 'un nom' : 'une date de naissance'
-            }
-
-            console.log(`Veuillez rentrer ${message}`)
-            res.writeHead(302, { 'Location': '/' });
-            res.end()
-            return
-          }
-
-          if(!isNaN(parseInt(name.trim()))) {
-            console.log(`Veuillez rentrer une chaine de caractères`)
-            res.writeHead(302, { 'Location': '/' });
-            res.end()
-            return
-          }
-
-          students.push({name, birth})
-          res.writeHead(302, { 'Location': '/' });
-          res.end();
-        });
-      }
     }
+  } else if (method === 'POST') {
+    if (req.url === '/') {
+      let body = '';
+
+      req.on('data', (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on('end', () => {
+
+        const formData = queryString.parse(body);
+        let name = formData.name;
+        let birth = formData.birth
+
+        verifyInput(res, name, birth, students)
+      });
+    }
+  }
 })
 
-server.listen('8888')
+server.listen(APP_PORT)
--- utils.js
+++ utils.js
@@ -2,12 +2,49 @@ import dayjs from 'dayjs';
 
 export const formattedArray = (array) => {
   let formattedStudentsArray = [...array];
+
   formattedStudentsArray = formattedStudentsArray.map(student => {
-    console.log('notFormatted', student.birth);
-    const formattedDate = dayjs(student.birth, 'YYYY-DD-MM').format('DD/MM/YYYY');
-    console.log('formatted',formattedDate )
+    let rearrangedDate = student.birth.replace(/(\d{4})-(\d{2})-(\d{2})/, '$1-$3-$2')
+    const formattedDate = dayjs(rearrangedDate, 'YYYY-MM-DD').format('DD/MM/YYYY');
     return { ...student, birth: formattedDate };
   });
 
   return formattedStudentsArray; 
+}
+
+export const verifyInput = (res,name, birth, students) => {
+  if (
+    name?.trim() === ''
+    || birth?.trim() === ''
+    || name?.trim() === '' && birth?.trim()
+  ) {
+    let message = ''
+    if (name?.trim() === '' && birth?.trim() === '') {
+      message = 'les 2 champs'
+    } else {
+      message = name?.trim() === '' ? 'un nom' : 'une date de naissance'
+    }
+
+    console.log(`Veuillez rentrer ${message}`)
+    res.writeHead(302, { 'Location': '/' });
+    res.end()
+  }
+
+  if (!isNaN(parseInt(name.trim()))) {
+    console.log(`Veuillez rentrer une chaine de caractères`)
+    res.writeHead(302, { 'Location': '/' });
+    res.end()
+  }
+
+  birth = dayjs(birth, 'YYYY-MM-DD').format('YYYY-DD-MM')
+
+  if(students.filter(student => student.name === name && student.birth === birth).length > 0) {
+    console.log(`L'élève existe déjà`)
+    res.writeHead(302, { 'Location': '/' });
+    res.end()
+  } else {
+    students.push({ name, birth })
+    res.writeHead(302, { 'Location': '/' });
+    res.end();
+  }
 }
